afbc27f19ba4edcfbaef18e0b919bd4def1311e1
farhan-qadir <farhanqadir.bscsf22@iba-suk.edu.pk>
2026-03-31 22:36:16 +0500

    updated the ui of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import homeIcon from '../assets/home.png';
+import bookIcon from '../assets/book.png';
+import loginIcon from '../assets/enter.png';
 import '../styles/Navbar.css';
 
 export default function Navbar() {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path ? 'active' : '';
+  };
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -15,18 +23,21 @@ export default function Navbar() {
         {/* Navigation Links */}
         <ul className="nav-menu">
           <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
+            <Link to="/" className={`nav-link ${isActive('/')}`}>
+              <img src={homeIcon} alt="Home" className="nav-icon" />
+              <span className="nav-text">Home</span>
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/add-book" className="nav-link">
-              Add Book
+            <Link to="/add-book" className={`nav-link ${isActive('/add-book')}`}>
+              <img src={bookIcon} alt="Add Book" className="nav-icon" />
+              <span className="nav-text">Add Book</span>
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/login" className="nav-link">
-              Login
+            <Link to="/login" className={`nav-link ${isActive('/login')}`}>
+              <img src={loginIcon} alt="Login" className="nav-icon" />
+              <span className="nav-text">Login</span>
             </Link>
           </li>
         </ul>
